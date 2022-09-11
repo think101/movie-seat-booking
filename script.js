0a6e81d64d1c612a41d0b7eb0d5e05268957f7c3
think101 <taohuayuan2000@hotmail.com>
2022-09-11 00:11:31 -0700

    fix bug

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ populateUI();
 
 let ticketPrice = +movieSelect.value;
 
+// save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
@@ -46,6 +52,7 @@ function populateUI() {
 // movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 
